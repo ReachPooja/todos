c575fc9627bc0b05350c608da70d6469adf5f3df
ReachPooja <reachpoojaprajapat@gmail.com>
2022-05-06 12:24:51 +0530

    feat: cleanup

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,19 +1,5 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter_test/flutter_test.dart';
-import 'package:todos/app/app.dart';
-import 'package:todos/counter/counter.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
-    });
-  });
+  group('App', () {});
 }
